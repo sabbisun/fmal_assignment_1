2d74cc7cde1302270bc658b5a51e46de920ffbea
kolli76 <loftrakinn@hotmail.com>
2017-02-01 14:14:11 +0000

    added inside main, only needed code

--- Compiler.cpp
+++ Compiler.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    Lexer myLexer = new Lexer();
+    Parser myParser = new Parser(myLexer);
+    myParser.parse();
     return 0;
 }
