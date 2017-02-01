c9a278ed211274e3556cefa0e777f2e4953d1f29
kolli76 <loftrakinn@hotmail.com>
2017-02-01 14:34:28 +0000

    added variables to token.h

---
+++ Token.h
@@ -0,0 +1,21 @@
+#ifndef TOKEN_H
+#define TOKEN_H
+#include<string>
+
+class Token
+{
+    public:
+        Token();
+        virtual ~Token();
+
+    protected:
+
+    private:
+
+        enum TokenCode { ID, ASSIGN, SEMICOL, INT, ADD, SUB, MULT, LPAREN, RPAREN, PRINT, END, ERROR }
+        String lexeme;
+        TokenCode tCode;
+
+};
+
+#endif // TEST_H
